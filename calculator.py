ad982e1cf81e5b44305092eb8506110822afc7a9
Dan Philip L. Achas <achas.danphilip@gmail.com>
2026-01-17 23:43:32 +0800

    Added Subtraction function and operator chooser.

--- calculator.py
+++ calculator.py
@@ -24,41 +24,61 @@ def main():
     history = load_history()
 
     print("Calculator")
-    print("Type 'q' to quit\n")
-
     while True:
-        first = input("Enter first number: ")
-        if first.lower() == "q":
-            break
+        #SELECTING OPERATION
+        print("Select an Operation:")
+        print("(1) Addition")
+        print("(2) Subtraction")
+        print("(0) Quit")
 
-        second = input("Enter second number: ")
-        if second.lower() == "q":
-            break
+        myInput : int
+        while True:
+            myInput = int(getInputNumber("> "))
+            if myInput >= 0 and myInput < 3: break
+            print("Please choose a number from 1 to 2.")
+        
+        if myInput == 0: break
 
-        try:
-            a = float(first)
-            b = float(second)
-            result = add(a, b)
+        firstNumber = getInputNumber("Enter first number: ")
+        secondNumber = getInputNumber("Enter second number: ")
+        result = 0
+        operation = 0
 
-            print(f"Result: {result}\n")
+        match myInput:
+            case 1:
+                operation = "addition"
+                result = add(firstNumber, secondNumber)
+                print(f"Result: {result}")
+            case 2:
+                operation = "subtraction"
+                result = subtract(firstNumber, secondNumber)
+                print(f"Result: {result}")
+            case 0: break
+            case _: print("Error") #A Choice not corresponding to any operation
 
-            history.append({
-                "operation": "addition",
-                "a": a,
-                "b": b,
-                "result": result
-            })
-
-            save_history(history)
-
-        except ValueError:
-            print("Invalid input. Please enter numbers.\n")
-
+        history.append({
+            "operation" : operation,
+            "a" : firstNumber,
+            "b" : secondNumber,
+            "result" : result
+        })
+        save_history(history)
+    
     print("Calculator closed. History saved.")
+
+def getInputNumber(prompt) -> float:
+    while True:
+        try: return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
 
 # ADDITION FUNCTION (AT THE BOTTOM)
 def add(a, b):
     return a + b
+
+# SUBTRACTION
+def subtract(a, b):
+    return a - b
 
 if __name__ == "__main__":
     main()
